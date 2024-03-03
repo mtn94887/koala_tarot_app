4453a1d8c237922918790e5beb7141f0fedc477c
Lwin Moe Aung <6531503151@lamduan.mfu.ac.th>
2024-03-03 13:45:32 +0700

    commited by HYun

---
+++ lib/home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:koala_tarot_app/tarot_card_page.dart';
+
+void main() {
+  runApp(TarotApp());
+}
+
+class TarotApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TarotHomePage(),
+    );
+  }
+}
+
+class TarotHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Choose the category",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Color(0xFF7D5AAD),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+          ),
+        ),
+        child: GridView.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 40,
+          crossAxisSpacing: 10,
+          children: [
+            CategoryCard(categoryName: "Education", iconData: Icons.school),
+            CategoryCard(categoryName: "Yes/No Question", iconData: Icons.sentiment_satisfied),
+            CategoryCard(categoryName: "Daily Reading", iconData: Icons.today),
+            CategoryCard(categoryName: "Weekly Reading", iconData: Icons.view_week),
+            CategoryCard(categoryName: "Quick Reading", iconData: Icons.flash_on),
+            CategoryCard(categoryName: "Three-Card Reading", iconData: Icons.format_list_numbered),
+            CategoryCard(categoryName: "Relationship Reading", iconData: Icons.favorite),
+            CategoryCard(categoryName: "New Love", iconData: Icons.energy_savings_leaf_sharp),
+            CategoryCard(categoryName: "Health", iconData: Icons.local_hospital),
+            CategoryCard(categoryName: "Prosperly", iconData: Icons.diamond),
+            CategoryCard(categoryName: "Money Career", iconData: Icons.account_balance_wallet),
+            CategoryCard(categoryName: "Background Problem Solution", iconData: Icons.blur_on),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article_outlined),
+            label: 'Spreads',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.self_improvement),
+            label: 'Meditation',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String categoryName;
+  final IconData iconData;
+
+  const CategoryCard({required this.categoryName, required this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TarotCardPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              categoryName,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tarot_card_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(TarotApp());
+}
+
+class TarotApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TarotCardPage(),
+    );
+  }
+}
+
+class TarotCardPage extends StatelessWidget {
+  final List<Map<String, String>> tarotCards = List.generate(
+    22,
+    (index) => {"name": "Card ${index + 1}", "image": "assets/card1.png"},
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final cardWidth = MediaQuery.of(context).size.width / 4;
+    final cardHeight = MediaQuery.of(context).size.height / 4;
+
+    tarotCards.shuffle();
+
+    return Scaffold(
+      appBar: AppBar(
+  title: Text(
+     "TAROT",
+    style: TextStyle(color: Colors.black),
+      ),
+  backgroundColor: Colors.white,
+  centerTitle: true, // Center the title horizontally
+),
+
+       // backgroundColor: Colors.white,
+    //  ),
+      body: GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 6,
+          crossAxisSpacing: 3,
+          mainAxisSpacing: 3,
+          childAspectRatio: cardWidth / cardHeight,
+        ),
+        itemCount: tarotCards.length,
+        itemBuilder: (context, index) {
+          return CardItem(
+            tarotCard: tarotCards[index],
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CardReadingPage(
+                    tarotCard: tarotCards[index]["name"]!,
+                    tarotCards: tarotCards,
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CardItem extends StatelessWidget {
+  final Map<String, String> tarotCard;
+  final VoidCallback onTap;
+
+  CardItem({required this.tarotCard, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Image.asset(
+          tarotCard["image"] ?? "assets/placeholder.png",
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
+
+class CardReadingPage extends StatelessWidget {
+  final String tarotCard;
+  final List<Map<String, String>> tarotCards;
+
+  CardReadingPage({required this.tarotCard, required this.tarotCards});
+
+  @override
+  Widget build(BuildContext context) {
+    tarotCards.shuffle();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tarotCard),
+        backgroundColor: Color(0xFF7D5AAD),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/c2.png',
+                width: 400,
+                height: 300,
+              ),
+              SizedBox(height: 90),
+             Text(
+                "The Empress graces your reading with a promise of abundant positivity. This card signifies a strong maternal presence, fostering harmony in relationships and signaling a potential expansion of your family or close social circle. "
+                "This is a time to embrace the nurturing aspects of life, recognizing the support and love that surround you. Stay open to the harmonious energies that The Empress brings, and allow them to guide you on this positive journey.",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.yellow, 
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
