29fb581a2d94a9db985fb80ea4f53522fd5c5c4e
Lwin Moe Aung <6531503151@lamduan.mfu.ac.th>
2024-04-14 10:35:00 +0700

    done

--- lib/tarot_card_page.dart
+++ lib/tarot_card_page.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:koala_tarot_app/home.dart';
 import 'dart:math';
-
-// class TarotApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: TarotCardPage(),
-//     );
-//   }
-// }
 
 class TarotCardPage extends StatelessWidget {
   final List<Map<String, String>> tarotCards = List.generate(
@@ -26,16 +16,13 @@ class TarotCardPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-  title: Text(
-     "TAROT",
-    style: TextStyle(color: Colors.black),
+        title: Text(
+          "TAROT",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
       ),
-  backgroundColor: Colors.white,
-  centerTitle: true, // Center the title horizontally
-),
-
-       // backgroundColor: Colors.white,
-    //  ),
       body: GridView.builder(
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
@@ -87,16 +74,24 @@ class CardItem extends StatelessWidget {
   }
 }
 
-
 class CardReadingPage extends StatelessWidget {
   final String tarotCard;
   final List<Map<String, String>> tarotCards;
+  final List<String> cardImages = [
+    'assets/image_0 copy.png',
+    'assets/image_1 copy.png',
+    'assets/image_2 copy.png',
+    'assets/image_3 copy.png',
+    'assets/image_4 copy.png',
+    'assets/image_5 copy.png',
+    'assets/image_6 copy.png',
+  ];
 
   CardReadingPage({required this.tarotCard, required this.tarotCards});
 
   @override
   Widget build(BuildContext context) {
-    tarotCards.shuffle();
+    int cardIndex = tarotCards.indexWhere((card) => card['name'] == tarotCard);
 
     return Scaffold(
       appBar: AppBar(
@@ -113,25 +108,25 @@ class CardReadingPage extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView( // Wrap your Column with SingleChildScrollView
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
-                  'assets/c2.png',
+                  cardImages[cardIndex],
                   width: 400,
                   height: 300,
                 ),
-                SizedBox(height: 50), // Adjusted this spacing
+                SizedBox(height: 50),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0), // Added horizontal padding
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: Text(
                     "The Empress graces your reading with a promise of abundant positivity. This card signifies a strong maternal presence, fostering harmony in relationships and signaling a potential expansion of your family or close social circle. "
                     "This is a time to embrace the nurturing aspects of life, recognizing the support and love that surround you. Stay open to the harmonious energies that The Empress brings, and allow them to guide you on this positive journey.",
                     style: TextStyle(
                       fontSize: 18,
-                      color: Colors.yellow, 
+                      color: Colors.yellow,
                     ),
                     textAlign: TextAlign.center,
                   ),
@@ -143,4 +138,10 @@ class CardReadingPage extends StatelessWidget {
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: TarotCardPage(),
+  ));
 }
