845e70f0fb816a941f4bcc52be2fd57067189ab3
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-04-21 13:20:15 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,31 @@ import 'package:koala_tarot_app/purpose.dart';
 import 'package:koala_tarot_app/features.dart';
 import 'package:koala_tarot_app/background.dart';
 import 'bottom_navigation_bar.dart'; 
+import 'package:firebase_core/firebase_core.dart'; 
 
-void main(){
-  runApp(
-    BottomNavigationBarExampleApp(), 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'key',
+      appId: 'id',
+      messagingSenderId: 'sendid',
+      projectId: 'myapp',
+      storageBucket: 'myapp-b9yt18.appspot.com',
+    )
   );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Your App Title',
+      home: Home(),
+      // You can add more MaterialApp configurations here
+    );
+  }
 }
 
 // class TarotApp extends StatelessWidget {
