7ecfe62cad5e0cca2d2c22d1a0943ef672eb7d5c
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-04-21 15:01:18 +0700

    done done done

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,84 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:koala_tarot_app/home.dart'; 
 
-class LoginPage extends StatefulWidget{
-  const LoginPage({Key?key}): super(key: key);
-  @override 
-  State<LoginPage> createState() => _LoginPageState(); 
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+      body: SafeArea(
+        child: Container(
+          //purple decoration bar 
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+            ),
           ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 100),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 70),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Gmail',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TarotHomePage()),
+                          );
+                        }, 
+                        child: Text('Continue'),
+                      ),
+                    ],
                   ),
                 ),
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                        Text(
-                          'Login',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 70),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Gmail',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => TarotHomePage()),
-                        );
-                      }, 
-                      child: Text('Continue'),
-                    ),
-                  ],
-                ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/signup.dart
+++ lib/signup.dart
@@ -24,121 +24,124 @@ class FormPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+      body: SafeArea(
+        child: Container(
+          //purple decoration bar
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+            ),
           ),
-        ),
-        child: Column(
-          children: [
-            SizedBox(height: 100),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+          child: SingleChildScrollView( // Wrap your Column with SingleChildScrollView
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
                   ),
-                ),
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                        Text(
-                          'Signup',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 40),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Full Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          Text(
+                            'Signup',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 40),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Full Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Gmail',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+                      SizedBox(height: 30),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Gmail',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+                      SizedBox(height: 30),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Confirm password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+                      SizedBox(height: 30),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Confirm password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 30),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 200,
-                          child: TextFormField(
-                            controller: _birthdayController,
-                            focusNode: _birthdayFocusNode,
-                            keyboardType: TextInputType.datetime,
-                            decoration: InputDecoration(
-                              hintText: 'dd/mm/yyyy',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
+                      SizedBox(height: 30),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 200,
+                            child: TextFormField(
+                              controller: _birthdayController,
+                              focusNode: _birthdayFocusNode,
+                              keyboardType: TextInputType.datetime,
+                              decoration: InputDecoration(
+                                hintText: 'dd/mm/yyyy',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(width: 40),
-                        // Calendar icon button to open date picker dialog
-                        IconButton(
-                          onPressed: () => _selectDate(context),
-                          icon: Icon(Icons.calendar_today),
-                          iconSize: 30,
-                          tooltip: 'Select Date',
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => TarotHomePage()),
-                        );
-                      }, 
-                      child: Text('Continue')
-                    ),
-                  ],
+                          SizedBox(width: 40),
+                          // Calendar icon button to open date picker dialog
+                          IconButton(
+                            onPressed: () => _selectDate(context),
+                            icon: Icon(Icons.calendar_today),
+                            iconSize: 30,
+                            tooltip: 'Select Date',
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TarotHomePage()),
+                          );
+                        }, 
+                        child: Text('Continue')
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
