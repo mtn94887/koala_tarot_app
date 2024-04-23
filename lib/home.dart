a99671124db47149ccf161e86dea9c101633932e
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-04-23 14:28:40 +0700

    nainai push

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:koala_tarot_app/ReadingsCategories/bgProblemSol.dart';
+import 'package:koala_tarot_app/ReadingsCategories/dailyReading.dart';
 import 'package:koala_tarot_app/ReadingsCategories/education.dart';
 import 'package:koala_tarot_app/ReadingsCategories/health.dart';
+import 'package:koala_tarot_app/ReadingsCategories/moneyCareer.dart';
+import 'package:koala_tarot_app/ReadingsCategories/newLove.dart';
+import 'package:koala_tarot_app/ReadingsCategories/prosperly.dart';
+import 'package:koala_tarot_app/ReadingsCategories/quickReading.dart';
 import 'package:koala_tarot_app/ReadingsCategories/relationshipReading.dart';
+import 'package:koala_tarot_app/ReadingsCategories/threeCardReading.dart';
+import 'package:koala_tarot_app/ReadingsCategories/weeklyReading.dart';
+import 'package:koala_tarot_app/ReadingsCategories/yes_noReading.dart';
 import 'package:koala_tarot_app/setting.dart';
 import 'package:koala_tarot_app/tarothistorypage.dart';
 import 'package:koala_tarot_app/meditationpage.dart';
@@ -69,15 +78,15 @@ class _TarotHomePageState extends State<TarotHomePage> {
             Education(name: "Education", iconData: Icons.school),
             Relationship(name: "Relationship Reading", iconData: Icons.favorite),
             Health(name: "Health", iconData: Icons.local_hospital),
-            // CategoryCard(categoryName: "Yes/No Question", iconData: Icons.sentiment_satisfied),
-            // CategoryCard(categoryName: "Daily Reading", iconData: Icons.today),
-            // CategoryCard(categoryName: "Weekly Reading", iconData: Icons.view_week),
-            // CategoryCard(categoryName: "Quick Reading", iconData: Icons.flash_on),
-            // CategoryCard(categoryName: "Three-Card Reading", iconData: Icons.format_list_numbered),
-            // CategoryCard(categoryName: "New Love", iconData: Icons.energy_savings_leaf_sharp),
-            // CategoryCard(categoryName: "Prosperly", iconData: Icons.diamond),
-            // CategoryCard(categoryName: "Money Career", iconData: Icons.account_balance_wallet),
-            // CategoryCard(categoryName: "Background Problem Solution", iconData: Icons.blur_on),
+            YesNo(name: "Yes/No Question", iconData: Icons.sentiment_satisfied),
+            DailyReading(name: "Daily Reading", iconData: Icons.today),
+            WeeklyReading(name: "Weekly Reading", iconData: Icons.view_week),
+            QuickReading(name: "Quick Reading", iconData: Icons.flash_on),
+            ThreeCardReading(name: "Three-Card Reading", iconData: Icons.format_list_numbered),
+            NewLove(name: "New Love", iconData: Icons.energy_savings_leaf_sharp),
+            Prosperly(name: "Prosperly", iconData: Icons.diamond),
+            MoneyCareer(name: "Money Career", iconData: Icons.account_balance_wallet),
+            BgProblemSol(name: "Background Problem Solution", iconData: Icons.blur_on),
           ],
         ),
       ),
@@ -212,6 +221,374 @@ class Health extends StatelessWidget{
 }
 
 
+//for yes no box ...................................
+class YesNo extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const YesNo({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => YesNoPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for daily reading box ...................................
+class DailyReading extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const DailyReading({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DailyReadingPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for weekly reading box ...................................
+class WeeklyReading extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const WeeklyReading({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => WeeklyReadingPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for quick reading box ...................................
+class QuickReading extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const QuickReading({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => QuickReadingPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for three card reading box ...................................
+class ThreeCardReading extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const ThreeCardReading({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ThreeCardReadingPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for new love box ...................................
+class NewLove extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const NewLove({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => NewLovePage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for prosperly box ...................................
+class Prosperly extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const Prosperly({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProsperlyPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for money career box ...................................
+class MoneyCareer extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const MoneyCareer({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MoneyCareerPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//for background problem solution box ...................................
+class BgProblemSol extends StatelessWidget{
+  final String name; 
+  final IconData iconData; 
+
+  const BgProblemSol({required this.name, required this.iconData});
+
+  @override 
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        // Navigate to the TarotCardPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BgProblemSolPage(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(height: 8),
+            Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
 // class CategoryCard extends StatelessWidget {
