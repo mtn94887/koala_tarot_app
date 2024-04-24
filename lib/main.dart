539d465bc721d26dcd1e778ff0a4e78e77e7ba59
SoeYuYuKyaw <6531503186@lamduan.mfu.ac.th>
2024-04-24 10:47:09 +0700

    history1

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:koala_tarot_app/cards.dart';
-import 'package:koala_tarot_app/home.dart';
 import 'package:koala_tarot_app/mainpage.dart';
-import 'package:koala_tarot_app/meditationpage.dart';
 import 'package:koala_tarot_app/tarothistorypage.dart';
 import 'package:koala_tarot_app/welcome.dart';
 import 'package:koala_tarot_app/purpose.dart';
 import 'package:koala_tarot_app/features.dart';
 import 'package:koala_tarot_app/background.dart';
-import 'bottom_navigation_bar.dart'; 
-import 'package:firebase_core/firebase_core.dart'; 
+import 'package:provider/provider.dart';
+import 'bottom_navigation_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    // options: FirebaseOptions(
-    //   apiKey: 'key',
-    //   appId: 'id',
-    //   messagingSenderId: 'sendid',
-    //   projectId: 'myapp',
-    //   storageBucket: 'myapp-b9yt18.appspot.com',
-    // )
-  );
-  runApp(MyApp());
+ WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+   // options: FirebaseOptions(
+   //   apiKey: 'key',
+   //   appId: 'id',
+   //   messagingSenderId: 'sendid',
+   //   projectId: 'myapp',
+   //   storageBucket: 'myapp-b9yt18.appspot.com',
+   // )
+ );
+ runApp(ChangeNotifierProvider(
+     create: (context) => TarotHistoryModel(),
+     child: MaterialApp(
+       home: MainPage(),
+     ),
+   ),
+ );
 }
 
+
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      //title: 'Your App Title',
-      home: MainPage(),
-      // You can add more MaterialApp configurations here
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     //title: 'Your App Title',
+     home: MainPage(),
+     // You can add more MaterialApp configurations here
+   );
+ }
 }
--- lib/tarothistorypage.dart
+++ lib/tarothistorypage.dart
@@ -1,45 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:koala_tarot_app/home.dart';
 import 'package:koala_tarot_app/meditationpage.dart';
 import 'package:koala_tarot_app/setting.dart';
 
-class tarothistorypage extends StatefulWidget {
-  @override
-  _tarothistorypageState createState() => _tarothistorypageState();
+
+class TarotCard {
+ final String image;
+ final String text;
+
+
+ TarotCard({required this.image, required this.text});
 }
 
-class _tarothistorypageState extends State<tarothistorypage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF7D5AAD),
-        automaticallyImplyLeading: false,
-        title: Text("Card Reading History"),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SettingScreen()),
-              );
-            },
-            icon: Icon(Icons.settings),
-            color: Colors.black,
-          )
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
-          ),
-        ),
-        // No GridView.builder here, removed the card display
-      ),
-    );
-  }
+
+class TarotHistoryModel extends ChangeNotifier {
+ final List<String> cardTexts = [
+   'Text for card 1',
+   'Text for card 2',
+   'Text for card 3',
+   // Add other card texts here
+ ];
+
+
+ List<TarotCard> _history = [];
+
+
+ List<TarotCard> get history => _history;
+
+
+ void addCard(TarotCard card) {
+   _history.add(card);
+   notifyListeners();
+ }
 }
+
+
+class tarothistorypage extends StatefulWidget {
+ @override
+ _tarothistorypageState createState() => _tarothistorypageState();
+}
+
+
+class _tarothistorypageState extends State<tarothistorypage> {
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       backgroundColor: Color(0xFF7D5AAD),
+       automaticallyImplyLeading: false,
+       title: Text("Card Reading History"),
+       centerTitle: true,
+       actions: <Widget>[
+         IconButton(
+           onPressed: () {
+             Navigator.push(
+               context,
+               MaterialPageRoute(builder: (context) => SettingScreen()),
+             );
+           },
+           icon: Icon(Icons.settings),
+           color: Colors.black,
+         )
+       ],
+     ),
+     body: Container(
+       decoration: BoxDecoration(
+         gradient: LinearGradient(
+           begin: Alignment.topCenter,
+           end: Alignment.bottomCenter,
+           colors: [Color(0xFF7D5AAD), Color(0xFF303E87)],
+         ),
+       ),
+       child: Consumer<TarotHistoryModel>(
+         builder: (context, historyModel, child) {
+           return ListView.builder(
+             itemCount: (historyModel.history.length / 3).ceil(),
+             itemBuilder: (context, columnIndex) {
+               return Padding(
+                 padding: const EdgeInsets.symmetric(vertical: 8.0),
+                 child: Row(
+                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                   children: List.generate(
+                     3,
+                     (index) {
+                       final cardIndex = columnIndex * 3 + index;
+                       if (cardIndex < historyModel.history.length) {
+                         final card = historyModel.history[cardIndex];
+                         return Expanded(
+                           child: Padding(
+                             padding: const EdgeInsets.all(8.0),
+                             child: Column(
+                               children: [
+                                 Image.asset(
+                                   card.image,
+                                   width: 70,
+                                 ),
+                                 SizedBox(height: 8),
+                                 Text(
+                                   "Education",
+                                   style: TextStyle(
+                                     color: Colors.black,
+                                     fontSize: 20, // Adjust the font size here
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ),
+                         );
+                       } else {
+                         return SizedBox(); // Placeholder for empty space
+                       }
+                     },
+                   ),
+                 ),
+               );
+             },
+           );
+         },
+       ),
+     ),
+   );
+ }
+}
+
+
