a61fa633120b8fa2296fb22d3476aceef4b585c2
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2024-04-25 11:52:29 +0700

    nainai

--- lib/Authenti/signup.dart
+++ lib/Authenti/signup.dart
@@ -6,6 +6,7 @@ import 'package:koala_tarot_app/home.dart';
 import 'package:koala_tarot_app/meditationpage.dart';
 import 'package:koala_tarot_app/setting.dart';
 import 'package:koala_tarot_app/tarothistorypage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; 
 
 
 class SignupPage extends StatefulWidget {
@@ -18,46 +19,65 @@ class SignupPage extends StatefulWidget {
 
 class _SignupPageState extends State<SignupPage> {
 
-  // //for date selection
-  // TextEditingController _birthdayController = TextEditingController();
-  // FocusNode _birthdayFocusNode = FocusNode();
-  // // Function to open date picker dialog
-  // Future<void> _selectDate(BuildContext context) async {
-  //   final DateTime? picked = await showDatePicker(
-  //     context: context,
-  //     initialDate: DateTime.now(),
-  //     firstDate: DateTime(1900),
-  //     lastDate: DateTime.now(),
-  //   );
-  //   if (picked != null) {
-  //     // Format the picked date as dd/mm/yyyy
-  //     String formattedDate = "${picked.day}/${picked.month}/${picked.year}";
-  //     // Update the text field with the selected date
-  //     _birthdayController.text = formattedDate;
-  //   }
-  // }
+  //for date selection
+  TextEditingController _birthdayController = TextEditingController();
+  FocusNode _birthdayFocusNode = FocusNode();
+  // Function to open date picker dialog
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      // Format the picked date as dd/mm/yyyy
+      String formattedDate = "${picked.day}/${picked.month}/${picked.year}";
+      // Update the text field with the selected date
+      _birthdayController.text = formattedDate;
+    }
+  }
 
 
   //firebase 
+  final _nameController = TextEditingController(); 
   final _emailController = TextEditingController(); 
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController(); 
+  //final _birthdayController = TextEditingController(); 
   @override 
   void dispose() { 
+    _nameController.dispose(); 
     _emailController.dispose(); 
     _passwordController.dispose(); 
     _confirmPasswordController.dispose(); 
+    _birthdayController.dispose(); 
     super.dispose(); 
   }
   //for firebase set up 
   Future signUp() async{
-    
     if (passwordConfirmed()){
+      //create user 
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
       email: _emailController.text.trim(), 
       password: _passwordController.text.trim(),
       );
+      //add user details 
+      addUserDetails(
+        _nameController.text.trim(),
+        _emailController.text.trim(),
+        int.parse(_birthdayController.text.trim()),
+      );
+
     }
+  }
+
+  Future addUserDetails(String name, String gmail, int birthday) async {
+    await FirebaseFirestore.instance.collection('users').add({
+      'name': '', 
+      'gmail': '',
+      'birthday': '',
+    });
   }
 
   bool passwordConfirmed() {
@@ -152,16 +172,17 @@ class _SignupPageState extends State<SignupPage> {
                         ],
                       ),
 
-                      // //full name text field 
-                      // SizedBox(height: 40),
-                      // TextField(
-                      //   decoration: InputDecoration(
-                      //     hintText: 'Full Name',
-                      //     border: OutlineInputBorder(
-                      //       borderRadius: BorderRadius.circular(20),
-                      //     ),
-                      //   ),
-                      // ),
+                      //full name text field 
+                      SizedBox(height: 40),
+                      TextField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          hintText: 'Full Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
 
                       //gmail text field 
                       SizedBox(height: 30),
@@ -199,72 +220,60 @@ class _SignupPageState extends State<SignupPage> {
                         ),
                       ),
 
-                      //the text for transition to the other page
-                      SizedBox(height: 5,),
+                      //birday selection text field 
+                      SizedBox(height: 30),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children:[
-                          Text(
-                            'A new user? ',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold, 
-                            )
+                        children: [
+                          SizedBox(
+                            width: 200,
+                            child: TextFormField(
+                              controller: _birthdayController,
+                              focusNode: _birthdayFocusNode,
+                              keyboardType: TextInputType.datetime,
+                              decoration: InputDecoration(
+                                hintText: 'dd/mm/yyyy',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
                           ),
-                          GestureDetector(
-                            onTap: widget.showLoginPage ,
-                            child: Text(
-                              'Register to use the app',
-                              style: TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.bold, 
-                              )
-                          )
-                          )
-                          
-                        ]
+                          SizedBox(width: 80),
+                          // Calendar icon button to open date picker dialog
+                          IconButton(
+                            onPressed: () => _selectDate(context),
+                            icon: Icon(Icons.calendar_today),
+                            iconSize: 30,
+                            tooltip: 'Select Date',
+                          ),
+                        ],
                       ),
 
 
-                      // //confirm passowrd text field 
-                      // SizedBox(height: 30),
-                      // TextField(
-                      //   decoration: InputDecoration(
-                      //     hintText: 'Confirm password',
-                      //     border: OutlineInputBorder(
-                      //       borderRadius: BorderRadius.circular(20),
-                      //     ),
-                      //   ),
-                      // ),
-
-
-                      // //birday selection text field 
-                      // SizedBox(height: 30),
+                      //the text for transition to the other page
+                      SizedBox(height: 5,),
                       // Row(
-                      //   children: [
-                      //     SizedBox(
-                      //       width: 200,
-                      //       child: TextFormField(
-                      //         controller: _birthdayController,
-                      //         focusNode: _birthdayFocusNode,
-                      //         keyboardType: TextInputType.datetime,
-                      //         decoration: InputDecoration(
-                      //           hintText: 'dd/mm/yyyy',
-                      //           border: OutlineInputBorder(
-                      //             borderRadius: BorderRadius.circular(20),
-                      //           ),
-                      //         ),
-                      //       ),
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children:[
+                      //     Text(
+                      //       'A new user? ',
+                      //       style: TextStyle(
+                      //         fontWeight: FontWeight.bold, 
+                      //       )
                       //     ),
-                      //     SizedBox(width: 40),
-                      //     // Calendar icon button to open date picker dialog
-                      //     IconButton(
-                      //       onPressed: () => _selectDate(context),
-                      //       icon: Icon(Icons.calendar_today),
-                      //       iconSize: 30,
-                      //       tooltip: 'Select Date',
-                      //     ),
-                      //   ],
-                      // ),
+                      //     GestureDetector(
+                      //       onTap: widget.showLoginPage ,
+                      //       child: Text(
+                      //         'Register to use the app',
+                      //         style: TextStyle(
+                      //           color: Colors.purple,
+                      //           fontWeight: FontWeight.bold, 
+                      //         )
+                      //     )
+                      //     )
+                          
+                      //   ]
+                      // ),  
 
                       //continue button
                       SizedBox(height: 20),
@@ -281,6 +290,8 @@ class _SignupPageState extends State<SignupPage> {
                         ),
                         child: Text('Sign Up')
                       ),
+
+                      SizedBox(height: 100,)
                     ],
                   ),
                 ),
@@ -298,3 +309,5 @@ class _SignupPageState extends State<SignupPage> {
 //     home: FormPage(),
 //   ));
 // }
+
+
